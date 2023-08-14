326b963b4c6970cf52e62d9b3aa11edac2d88e58
marcvanduyn <marcvanduyn1@gmail.com>
2023-08-14 21:48:14 +0200

    Fix portfolio configuration

--- finterion_investing_algorithm_framework/models/portfolio_configuration.py
+++ finterion_investing_algorithm_framework/models/portfolio_configuration.py
@@ -1,15 +1,19 @@
 from investing_algorithm_framework import PortfolioConfiguration
+from investing_algorithm_framework.domain import ImproperlyConfigured
 
 
 class FinterionPortfolioConfiguration(PortfolioConfiguration):
 
     def __init__(self, api_key, trading_symbol):
-        super().__init__(
-            market="finterion",
-            trading_symbol=trading_symbol,
-            api_key=api_key,
-            secret_key=None,
-        )
+        try:
+            super().__init__(
+                market="finterion",
+                trading_symbol=trading_symbol,
+                api_key=api_key,
+                secret_key=None,
+            )
+        except ImproperlyConfigured:
+            pass
 
     @property
     def identifier(self):
